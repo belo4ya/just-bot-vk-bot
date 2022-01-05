263761d1592672571807c257b252628204db5621
belo4ya <41exey.kov41ev@gmail.com>
2022-01-05 18:25:07 +0300

    vk bot + cron scheduler

--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module github.com/belo4ya/just-bot-vk-bot
 go 1.17
 
 require (
-	github.com/SevereCloud/vksdk/v2 v2.11.0 // indirect
+	github.com/SevereCloud/vksdk/v2 v2.11.0
+	github.com/go-joe/joe v0.11.0
+	github.com/joho/godotenv v1.4.0
+	github.com/robfig/cron/v3 v3.0.0
+	github.com/tdakkota/joe-vk-adapter v0.0.0-20211015093436-30def448ed57
+)
+
+require (
+	github.com/SevereCloud/vksdk v1.10.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,51 +1,54 @@
 package main
 
 import (
-	"context"
+	"github.com/go-joe/joe"
+	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 	"log"
+	"os"
+	"time"
 
-	"github.com/SevereCloud/vksdk/v2/api"
-	"github.com/SevereCloud/vksdk/v2/api/params"
-	"github.com/SevereCloud/vksdk/v2/events"
-	"github.com/SevereCloud/vksdk/v2/longpoll-bot"
+	joeVk "github.com/tdakkota/joe-vk-adapter"
 )
 
-func main() {
-	token := "<TOKEN>" // use os.Getenv("TOKEN")
-	vk := api.NewVK(token)
+func registerHandlers(b *joe.Bot) {
+	b.Respond("ping", func(msg joe.Message) error {
+		msg.Respond("pong")
+		return nil
+	})
+}
 
-	// get information about the group
-	group, err := vk.GroupsGetByID(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+func registerCron(b *joe.Bot) {
+	vkAdapter := b.Adapter.(*joeVk.BotAdapter)
 
-	// Initializing Long Poll
-	lp, err := longpoll.NewLongPoll(vk, group[0].ID)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// New message event
-	lp.MessageNew(func(_ context.Context, obj events.MessageNewObject) {
-		log.Printf("%d: %s", obj.Message.PeerID, obj.Message.Text)
-
-		if obj.Message.Text == "ping" {
-			b := params.NewMessagesSendBuilder()
-			b.Message("pong")
-			b.RandomID(0)
-			b.PeerID(obj.Message.PeerID)
-
-			_, err := vk.MessagesSend(b.Params)
-			if err != nil {
-				log.Fatal(err)
-			}
+	c := cron.New()
+	entryID, err := c.AddFunc("@every 15s", func() {
+		if err := vkAdapter.Send(time.Now().Local().Format("15:04 02.01.2006"), "510253487"); err != nil {
+			b.Logger.Fatal(err.Error())
 		}
 	})
+	if err != nil {
+		b.Logger.Fatal(err.Error())
+	}
+	log.Printf("EntryID: %d", entryID)
+	c.Start()
+}
 
-	// Run Bots Long Poll
-	log.Println("Start Long Poll")
-	if err := lp.Run(); err != nil {
-		log.Fatal(err)
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("TOKEN")
+	log.Printf("TOKEN: %s", token)
+
+	b := joe.New("just-bot", joeVk.Adapter(token))
+
+	registerCron(b)
+	registerHandlers(b)
+
+	if err := b.Run(); err != nil {
+		b.Logger.Fatal(err.Error())
 	}
 }
